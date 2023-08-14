3db9fa9854ebeb87ce9995ff593802444954d7c4
Nicolas Cayez <130738851+NicolasCayez@users.noreply.github.com>
2023-08-14 16:42:52 +0200

    amélioration recherche sur les auteurs

--- Projet requête recherche v2.sql
+++ Projet requête recherche v2.sql
@@ -15,25 +15,30 @@ SELECT 'Tome' AS 'Type', T.tome_id AS 'Id', S.serie_nom AS 'Série', T.tome_nume
 FROM tomes AS T
 LEFT JOIN series AS S
 ON T.serie_id = S.serie_id
-WHERE
-T.tome_id IN(
+WHERE T.tome_id IN(
 	SELECT T.tome_id
-FROM tomes AS T
-WHERE T.tome_titre like @search
-OR T.tome_isbn like @search
-OR T.tome_edition_speciale_libelle like @search
+	FROM tomes AS T
+	WHERE T.tome_titre like @search
+	OR T.tome_isbn like @search
+	OR T.tome_edition_speciale_libelle like @search
 )
 UNION
 SELECT 'Série', S.serie_id, S.serie_nom, NULL, NULL, NULL
 FROM series AS S
-HAVING S.serie_nom like @search
+WHERE S.serie_nom like @search
 UNION
 SELECT 'Editeur', E.editeur_id, NULL, NULL, E.editeur_nom, NULL
 FROM editeurs AS E
-HAVING E.editeur_nom like @search
+WHERE E.editeur_nom like @search
 UNION
 SELECT DISTINCT 'Auteur', A.auteur_id, NULL, NULL, A.auteur_pseudo, NULL
 FROM auteurs AS A
-HAVING A.auteur_pseudo like @search
+WHERE A.auteur_id IN(
+	SELECT A.auteur_id
+	FROM auteurs AS A
+	WHERE A.auteur_nom like @search
+	OR A.auteur_prenom like @search
+	OR A.auteur_pseudo like @search
+)
 ORDER BY TYPE
 ;
