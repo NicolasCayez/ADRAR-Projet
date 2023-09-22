24c302cce4370e0f4e94c8ee939e9d4267a492c0
Nicolas Cayez <130738851+NicolasCayez@users.noreply.github.com>
2023-09-22 11:35:47 +0200

    presentation ok

--- structureBody.js
+++ structureBody.js
@@ -10,84 +10,180 @@ menuLateral.append(titreMenuLateral);
 let menuLateralElements = document.createElement("ul");
 menuLateralElements.classList.add("menu-lateral-elements");
 menuLateral.appendChild(menuLateralElements);
+
+function creerElementMenuLateral (idChild, libelle) {
+    let lienLi = document.createElement("li");
+    menuLateralElements.appendChild(lienLi);
+    let btnLien = document.createElement("p");
+    btnLien.classList.add("btnMenuLateral");
+    btnLien.id = idChild;
+    btnLien.innerText = libelle;
+    lienLi.appendChild(btnLien);
+}
+
 if (menuLateral.id = "menuLateralPresentation") {
     //Bouton Accueil
-    let liLienAccueil = document.createElement("li");
-    menuLateralElements.appendChild(liLienAccueil);
-    let btnLienAccueil = document.createElement("p");
-    btnLienAccueil.classList.add("btnMenuLateral");
-    btnLienAccueil.id = "btnLienAccueil";
-    btnLienAccueil.innerText = "Accueil";
-    liLienAccueil.appendChild(btnLienAccueil);
+    creerElementMenuLateral("btnLienAccueil","Accueil");
     //Bouton Collection 
-    let liLienCollection = document.createElement("li");
-    menuLateralElements.appendChild(liLienCollection);
-    let btnLienCollection = document.createElement("p");
-    btnLienCollection.classList.add("btnMenuLateral");
-    btnLienCollection.id = "btnLienCollection";
-    btnLienCollection.innerText = "Collection";
-    liLienCollection.appendChild(btnLienCollection);
+    creerElementMenuLateral("btnLienCollection","Collection");
     //Bouton Series
-    let liLienSeries = document.createElement("li");
-    menuLateralElements.appendChild(liLienSeries);
-    let btnLienSeries = document.createElement("p");
-    btnLienSeries.classList.add("btnMenuLateral");
-    btnLienSeries.id = "btnLienSeries";
-    btnLienSeries.innerText = "Séries";
-    liLienSeries.appendChild(btnLienSeries);
+    creerElementMenuLateral("btnLienSeries","Séries");
     //Bouton Tomes
-    let liLienTomes = document.createElement("li");
-    menuLateralElements.appendChild(liLienTomes);
-    let btnLienTomes = document.createElement("p");
-    btnLienTomes.classList.add("btnMenuLateral");
-    btnLienTomes.id = "btnLienTomes";
-    btnLienTomes.innerText = "Tomes";
-    liLienTomes.appendChild(btnLienTomes);
+    creerElementMenuLateral("btnLienTomes","Tomes");
     //Bouton Auteurs
-    let liLienAuteurs = document.createElement("li");
-    menuLateralElements.appendChild(liLienAuteurs);
-    let btnLienAuteurs = document.createElement("p");
-    btnLienAuteurs.classList.add("btnMenuLateral");
-    btnLienAuteurs.id = "btnLienAuteurs";
-    btnLienAuteurs.innerText = "Auteurs";
-    liLienAuteurs.appendChild(btnLienAuteurs);
+    creerElementMenuLateral("btnLienAuteurs","Auteurs");
     //Bouton Editeurs
-    let liLienEditeurs = document.createElement("li");
-    menuLateralElements.appendChild(liLienEditeurs);
-    let btnLienEditeurs = document.createElement("p");
-    btnLienEditeurs.classList.add("btnMenuLateral");
-    btnLienEditeurs.id = "btnLienSerie";
-    btnLienEditeurs.innerText = "Editeurs";
-    liLienEditeurs.appendChild(btnLienEditeurs);
+    creerElementMenuLateral("btnLienEditeurs","Editeurs");
     //Bouton Réglages
-    let liLienReglages = document.createElement("li");
-    menuLateralElements.appendChild(liLienReglages);
-    let btnLienReglages = document.createElement("p");
-    btnLienReglages.classList.add("btnMenuLateral");
-    btnLienReglages.id = "btnLienReglages";
-    btnLienReglages.innerText = "Collection";
-    liLienReglages.appendChild(btnLienReglages);
+    creerElementMenuLateral("btnLienReglages","Réglages");
     //Bouton A propos
-    let liLienAPropos = document.createElement("li");
-    menuLateralElements.appendChild(liLienAPropos);
-    let btnLienAPropos = document.createElement("p");
-    btnLienbtnLienAProposSerie.classList.add("btnMenuLateral");
-    btnLienAPropos.id = "btnLienSerie";
-    btnLienAPropos.innerText = "A Propos";
-    liLienAPropos.appendChild(btnLienAPropos);
+    creerElementMenuLateral("btnLienAPropos","A Propos");
+};
+
+//variables de repères sur la page
+
+
+
+
+// Chargement de la page par défaut
+let cadreAndroid = document.querySelector(".cadreAndroid");
+// textes
+const texteMaquetteAccueil = document.getElementById("texteAccueil");
+const texteMaquetteCollection = document.getElementById("texteCollection");
+const texteMaquetteSeries = document.getElementById("texteSeries");
+const texteMaquetteTomes = document.getElementById("texteTomes");
+const texteMaquetteAuteurs = document.getElementById("texteAuteurs");
+const texteMaquetteEditeurs = document.getElementById("texteEditeurs");
+const texteMaquetteReglages = document.getElementById("texteReglages");
+const texteMaquetteApropos = document.getElementById("texteAPropos");
+// boutons
+let boutonMaquetteAccueil = document.getElementById("btnLienAccueil");
+let boutonMaquetteCollection = document.getElementById("btnLienCollection");
+let boutonMaquetteSeries = document.getElementById("btnLienSeries");
+let boutonMaquetteTomes = document.getElementById("btnLienTomes");
+let boutonMaquetteAuteurs = document.getElementById("btnLienAuteurs");
+let boutonMaquetteEditeurs = document.getElementById("btnLienEditeurs");
+let boutonMaquetteReglages = document.getElementById("btnLienReglages");
+let boutonMaquetteAPropos = document.getElementById("btnLienAPropos");
+// images des cadres
+const imagecadreAndroidAccueil = document.createElement("img");
+const imagecadreAndroidPresentation = document.createElement("img");
+
+
+switch (cadreAndroid.id) {
+    case "cadreAndroidAccueil" :
+        imagecadreAndroidAccueil.src = "./img/MaquetteAccueil.png";
+        cadreAndroid.appendChild(imagecadreAndroidAccueil);
+    break;
+    case "cadreAndroidPresentation" :
+        // let imagecadreAndroidPresentation = document.createElement("img");
+        imagecadreAndroidPresentation.src = "./img/MaquetteCollection.png";
+        cadreAndroid.appendChild(imagecadreAndroidPresentation);
+        texteMaquetteAccueil.style.display = "inline-block";
+    break;
 };
 
 
-// Cadre android page Présentation
+// Clic bouton Menu Latéral Accueil
+boutonMaquetteAccueil.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "inline-block";
+    texteMaquetteCollection.style.display = "none";
+    texteMaquetteSeries.style.display = "none";
+    texteMaquetteTomes.style.display = "none";
+    texteMaquetteAuteurs.style.display = "none";
+    texteMaquetteEditeurs.style.display = "none";
+    texteMaquetteReglages.style.display = "none";
+    texteMaquetteApropos.style.display = "none";
+    imagecadreAndroidPresentation.src = "./img/MaquetteAccueil.png";
 
-// defaut accueil
-// if sur clic bouton, changer image(s)
+});
+// Clic bouton Menu Latéral Collection
+boutonMaquetteCollection.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "none";
+    texteMaquetteCollection.style.display = "inline-block";
+    texteMaquetteSeries.style.display = "none";
+    texteMaquetteTomes.style.display = "none";
+    texteMaquetteAuteurs.style.display = "none";
+    texteMaquetteEditeurs.style.display = "none";
+    texteMaquetteReglages.style.display = "none";
+    texteMaquetteApropos.style.display = "none";
+    imagecadreAndroidPresentation.src = "./img/MaquetteCollection.png";
 
+});
+// Clic bouton Menu Latéral Series
+boutonMaquetteSeries.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "none";
+    texteMaquetteCollection.style.display = "none";
+    texteMaquetteSeries.style.display = "inline-block";
+    texteMaquetteTomes.style.display = "none";
+    texteMaquetteAuteurs.style.display = "none";
+    texteMaquetteEditeurs.style.display = "none";
+    texteMaquetteReglages.style.display = "none";
+    texteMaquetteApropos.style.display = "none";
+    imagecadreAndroidPresentation.src = "./img/MaquetteSeries.png";
 
+});
+// Clic bouton Menu Latéral Tomes
+boutonMaquetteTomes.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "none";
+    texteMaquetteCollection.style.display = "none";
+    texteMaquetteSeries.style.display = "none";
+    texteMaquetteTomes.style.display = "inline-block";
+    texteMaquetteAuteurs.style.display = "none";
+    texteMaquetteEditeurs.style.display = "none";
+    texteMaquetteReglages.style.display = "none";
+    texteMaquetteApropos.style.display = "none";
+    imagecadreAndroidPresentation.src = "./img/MaquetteTomes.png";
 
+});
+// Clic bouton Menu Latéral Auteurs
+boutonMaquetteAuteurs.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "none";
+    texteMaquetteCollection.style.display = "none";
+    texteMaquetteSeries.style.display = "none";
+    texteMaquetteTomes.style.display = "none";
+    texteMaquetteAuteurs.style.display = "inline-block";
+    texteMaquetteEditeurs.style.display = "none";
+    texteMaquetteReglages.style.display = "none";
+    texteMaquetteApropos.style.display = "none";
+    imagecadreAndroidPresentation.src = "./img/MaquetteAuteurs.png";
 
+});
+// Clic bouton Menu Latéral Editeurs
+boutonMaquetteEditeurs.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "none";
+    texteMaquetteCollection.style.display = "none";
+    texteMaquetteSeries.style.display = "none";
+    texteMaquetteTomes.style.display = "none";
+    texteMaquetteAuteurs.style.display = "none";
+    texteMaquetteEditeurs.style.display = "inline-block";
+    texteMaquetteReglages.style.display = "none";
+    texteMaquetteApropos.style.display = "none";
+    imagecadreAndroidPresentation.src = "./img/MaquetteEditeurs.png";
+});
+// Clic bouton Menu Latéral Réglages
+boutonMaquetteReglages.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "none";
+    texteMaquetteCollection.style.display = "none";
+    texteMaquetteSeries.style.display = "none";
+    texteMaquetteTomes.style.display = "none";
+    texteMaquetteAuteurs.style.display = "none";
+    texteMaquetteEditeurs.style.display = "none";
+    texteMaquetteReglages.style.display = "inline-block";
+    texteMaquetteApropos.style.display = "none";
+    imagecadreAndroidPresentation.src = "./img/MaquetteReglages.png";
 
-let cadreAndroidPresentation = document.getElementById("cadreAndroidPresentation");
-let imagecadreAndroidPresentation = document.createElement("img");
-imagecadreAndroidPresentation.src = "./img/MaquetteAccueil.png";
-cadreAndroidPresentation.appendChild(imagecadreAndroidPresentation);
+});
+// Clic bouton Menu Latéral A Propos
+boutonMaquetteAPropos.addEventListener('click', () => {
+    texteMaquetteAccueil.style.display = "none";
+    texteMaquetteCollection.style.display = "none";
+    texteMaquetteSeries.style.display = "none";
+    texteMaquetteTomes.style.display = "none";
+    texteMaquetteAuteurs.style.display = "none";
+    texteMaquetteEditeurs.style.display = "none";
+    texteMaquetteReglages.style.display = "none";
+    texteMaquetteApropos.style.display = "inline-block";
+    imagecadreAndroidPresentation.src = "./img/MaquetteAPropos.png";
+
+});
